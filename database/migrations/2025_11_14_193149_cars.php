8c4f4aeca2374a7608e0fe4d8fc023c2ba836b95
davidberalcagit <2904619@alu.murciaeduca.es>
2025-11-18 08:54:01 +0100

    Marcas_Modelos

--- database/migrations/2025_11_14_193149_cars.php
+++ database/migrations/2025_11_14_193149_cars.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_vendedor')->constrained('sellers')->onDelete('cascade');
+            $table->foreignId('id_marca')->constrained('models')->onDelete('cascade');
             $table->string('marca');
             $table->string('modelo');
             $table->string('matricula');
